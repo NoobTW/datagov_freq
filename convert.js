4916d7c6e6fc2391bcbfad71f98600c37e98c33c
NoobTW <noob@noob.tw>
2018-01-07 22:52:38 +0800

    update: local goverments only

--- convert.js
+++ convert.js
@@ -27,34 +27,87 @@ axios.get('https://data.gov.tw/datasets/export/csv')
 			});
 		});
 
+		const final = [];
 		Array.from(categoriesArr).forEach(d => {
 			const n = d.name;
-			if(n === ''){
-				d.group = '未命名';
-			}
-			else if(n.includes('府') || n.includes('縣') || n.includes('公所') || n.includes('省') ||
-			(n.includes('縣') && n.includes('局')) || (n.includes('市') && n.includes('局')) ||
-			(n.includes('縣') && n.includes('處')) || (n.includes('市') && n.includes('處')) ||
-			n.includes('桃園') || n.includes('臺北') || n.includes('臺中') || n.includes('衛武營')){
 
-				d.group = '地方政府';
-			}else if(n.includes('大學')){
-				d.group = '大學';
-			}else if(n.includes('公司')){
-				d.group = '國營事業/官股公司';
-			}else if(n.includes('部') || n.includes('院') || n.includes('委員會') ||
-			n.includes('中央') || n.includes('銀行') || n.includes('國立') || n.includes('處') || n.includes('中心') ||
-			n.includes('司') || n.includes('局') || n.includes('署') || n.includes('室') || n.includes('館') || n.includes('會議')){
-				d.group = '中央政府';
-			}else{
-				d.group = '未分類';
+			if(n.includes('臺北')){
+				d.group = '臺北市';
+			}else if(n.includes('雲林')){
+				d.group = '雲林縣';
+			}else if(n.includes('屏東')){
+				d.group = '屏東縣';
+			}else if(n.includes('新北')){
+				d.group = '新北市';
+			}else if(n.includes('基隆')){
+				d.group = '基隆市';
+			}else if(n.includes('臺東')){
+				d.group = '臺東縣';
+			}else if(n.includes('桃園')){
+				d.group = '桃園市';
+			}else if(n.includes('嘉義縣')){
+				d.group = '嘉義縣';
+			}else if(n.includes('花蓮')){
+				d.group = '花蓮縣';
+			}else if(n.includes('新竹縣')){
+				d.group = '新竹縣';
+			}else if(n.includes('嘉義市')){
+				d.group = '嘉義市';
+			}else if(n.includes('金門縣')){
+				d.group = '金門縣';
+			}else if(n.includes('新竹市')){
+				d.group = '新竹市';
+			}else if(n.includes('臺南市')){
+				d.group = '臺南市';
+			}else if(n.includes('澎湖')){
+				d.group = '澎湖縣';
+			}else if(n.includes('宜蘭')){
+				d.group = '宜蘭縣';
+			}else if(n.includes('高雄')){
+				d.group = '高雄市';
+			}else if(n.includes('臺中')){
+				d.group = '臺中市';
+			}else if(n.includes('臺南')){
+				d.group = '臺南市';
+			}else if(n.includes('苗栗縣')){
+				d.group = '苗栗縣';
+			}else if(n.includes('彰化縣')){
+				d.group = '彰化縣';
+			}else if(n.includes('南投縣')){
+				d.group = '南投縣';
 			}
-		})
 
-		categoriesArr.sort((a, b) => b.value - a.value);
+			if(d.group) final.push(d);
+
+			// const n = d.name;
+			// if(n === ''){
+			// 	d.group = '未命名';
+			// }
+			// else if(n.includes('府') || n.includes('縣') || n.includes('公所') || n.includes('省') ||
+			// (n.includes('縣') && n.includes('局')) || (n.includes('市') && n.includes('局')) ||
+			// (n.includes('縣') && n.includes('處')) || (n.includes('市') && n.includes('處')) ||
+			// n.includes('桃園') || n.includes('臺北') || n.includes('臺中') || n.includes('衛武營')){
+
+			// 	d.group = '地方政府';
+			// }else if(n.includes('大學')){
+			// 	d.group = '大學';
+			// }else if(n.includes('公司')){
+			// 	d.group = '國營事業/官股公司';
+			// }else if(n.includes('部') || n.includes('院') || n.includes('委員會') ||
+			// n.includes('中央') || n.includes('銀行') || n.includes('國立') || n.includes('處') || n.includes('中心') ||
+			// n.includes('司') || n.includes('局') || n.includes('署') || n.includes('室') || n.includes('館') || n.includes('會議')){
+			// 	d.group = '中央政府';
+			// }else{
+			// 	d.group = '未分類';
+			// }
+		});
+
+		console.log(final);
+
+		final.sort((a, b) => b.value - a.value);
 
 		const data = {
-			content: categoriesArr,
+			content: final,
 			lastModified: new Date(),
 		}
 
--- docs/all.js
+++ docs/all.js
@@ -6,27 +6,36 @@ $(function(){
 		console.log(res.lastModified);
 		$('#lm_date').text(moment(new Date(res.lastModified)).format('YYYY-MM-DD'));
 
+		var visulization;
+
 		if(getParameterByName('cat') && getParameterByName('cat').trim() !== ''){
 			sample_data = res.content.filter(function(x){
 				return x.group === getParameterByName('cat');
 			});
+			visualization = d3plus.viz()
+				.container("#viz")
+				.data(sample_data)
+				.type("tree_map")
+				.id("name")
+				.size("value")
+				.color("name")
+				.draw()
+		}else{
+			visualization = d3plus.viz()
+				.container("#viz")
+				.data(sample_data)
+				.type("tree_map")
+				.id("group")
+				.size("value")
+				.color("group")
+				.draw()
 		}
 
-		var visualization = d3plus.viz()
-		.container("#viz")
-		.data(sample_data)
-		.type("bubbles")
-		.id(["group", "name"])
-		.depth(1)
-		.size("value")
-		.color("name")
-		.background("#F2F2F2")
-		.draw()
 	});
 });
 
 $('body').delegate('text', 'click', function(){
-	if($(this).attr('id').startsWith('d3plus_label_null'))
+	if($(this).attr('id').startsWith('d3plus_label') && $(this).text().length === 3)
 		location.href = location.protocol + '//' + location.host + location.pathname + '?cat=' + $(this).text();
 })
 
