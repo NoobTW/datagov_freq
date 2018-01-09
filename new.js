c07faf53a93941f68bd8aeefc6f0307ac17fd920
NoobTW <noob@noob.tw>
2018-01-09 19:04:29 +0800

    fix local data

---
+++ new.js
@@ -0,0 +1,47 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+
+const content = [];
+
+csv()
+.fromFile('./all.utf8.csv')
+.on('json', (d) => {
+	if(d['編號'] !== '編號'){
+		d.name = d['第一層級'] + d['第二層級'];
+		content.push(d);
+	}
+}).on('done', (err) => {
+	console.log(content.length);
+	const categories = {};
+	console.log('Analyzing......');
+	Array.from(content).forEach(dataset => {
+		categories[dataset.name] = categories.hasOwnProperty(dataset.name) ? categories[dataset.name] + 1 : 1;
+	});
+
+	// console.log(categories);
+
+	const categoriesArr = [];
+	Object.keys(categories).forEach((key) => {
+		if(key && key.trim() !== ''){
+			categoriesArr.push({
+				name: key,
+				value: categories[key],
+				group: key.substring(0, 3),
+			});
+		}
+	});
+
+	console.log(categoriesArr);
+
+	// const final = [];
+
+	// console.log(final);
+
+	categoriesArr.sort((a, b) => b.value - a.value);
+
+	const data = {
+		content: categoriesArr,
+		lastModified: new Date(),
+	}
+	fs.writeFileSync('./docs/data/data22.json', JSON.stringify(data), 'UTF-8');
+});
